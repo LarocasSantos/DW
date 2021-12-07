10c0baa81ac75162ca6d0002dfa3d06137b2b306
Avramgincu <58823292+Avramgincu@users.noreply.github.com>
2021-12-07 17:52:25 +0000

    accordion js

---
+++ js/index.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+    // link action
+    $(".action").click(function(e){
+        e.preventDefault();
+
+        $(".slide").removeClass("active");
+        var slide = $(this).closest(".slide");
+        slide.addClass("active");
+    });
+
+});
+
+/*
+window.onload = function(){
+    var hideMe = document.getElementById('slide');
+    document.onclick = function(e){
+       if(e.target.id !== 'slide'){
+          //hideMe.style.display = 'none';
+          $(".slide").removeClass("active");
+       }
+       else{
+        var slide = $(this).closest(".slide");
+        slide.addClass("active");
+       }
+    };
+ };
+*/
+
+// checkWidth
+checkWidth = function(){
+    var windowsize = $(window).width();
+    if (windowsize > 480) {
+        var slideWidth = $('.active').width();
+        $('.slide-content').css({
+            "width" : slideWidth+"px"
+        });
+    }
+    
+}
+
+$(window).resize(function() {
+    // onresize
+    checkWidth();
+
+    // finish resize
+    clearTimeout(window.resizedFinished);
+    window.resizedFinished = setTimeout( checkWidth , 500);
+});
